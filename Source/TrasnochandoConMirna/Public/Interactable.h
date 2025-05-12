1d61a32f1409886c97745e25ff692a870b8b7a8b
cabranca <joaquincabrera98@gmail.com>
2025-05-12 19:55:19 -0300

    Added Interactable interface

---
+++ Source/TrasnochandoConMirna/Public/Interactable.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "Interactable.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UInteractable : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class TRASNOCHANDOCONMIRNA_API IInteractable
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+
+	UFUNCTION(Server, Reliable)
+	virtual void ServerInteract() = 0;
+
+	virtual bool IsGrabbable() = 0;
+};
