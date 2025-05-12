ccbb269806fa7850622f78f20f5ce72fbc22f952
cabranca <joaquincabrera98@gmail.com>
2025-05-12 19:22:06 -0300

    Added Puzzle Interface

---
+++ Source/TrasnochandoConMirna/Private/Puzzle.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Puzzle.h"
+
+// Add default functionality here for any IPuzzle functions that are not pure virtual.
