5b8d4792dc5c3627c93bcae255830f113dd5d81c
cabranca <joaquincabrera98@gmail.com>
2025-05-23 19:03:55 -0300

    Added simple animator component

---
+++ Source/TrasnochandoConMirna/Private/SimpleAnimatorComponent.cpp
@@ -0,0 +1,62 @@
+#include "SimpleAnimatorComponent.h"
+
+USimpleAnimatorComponent::USimpleAnimatorComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+
+// Called when the game starts
+void USimpleAnimatorComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (TargetMesh)
+	{
+		TargetMesh->SetRelativeLocation(StartLocation);
+	}
+}
+
+
+// Called every frame
+void USimpleAnimatorComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+    if (!bIsAnimating || !TargetMesh)
+        return;
+
+    ElapsedTime += DeltaTime;
+    float Alpha = FMath::Clamp(ElapsedTime / AnimationDuration, 0.0f, 1.0f);
+
+    FVector NewLocation = bForward
+        ? FMath::Lerp(StartLocation, EndLocation, Alpha)
+        : FMath::Lerp(EndLocation, StartLocation, Alpha);
+
+    TargetMesh->SetRelativeLocation(NewLocation);
+
+    if (Alpha >= 1.0f)
+    {
+        bIsAnimating = false;
+    }
+}
+
+void USimpleAnimatorComponent::PlayForward()
+{
+    if (TargetMesh && AnimationDuration > 0)
+    {
+        bIsAnimating = true;
+        bForward = true;
+        ElapsedTime = 0.0f;
+    }
+}
+
+void USimpleAnimatorComponent::PlayReverse()
+{
+    if (TargetMesh && AnimationDuration > 0)
+    {
+        bIsAnimating = true;
+        bForward = false;
+        ElapsedTime = 0.0f;
+    }
+}
---
+++ Source/TrasnochandoConMirna/Public/SimpleAnimatorComponent.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "SimpleAnimatorComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class TRASNOCHANDOCONMIRNA_API USimpleAnimatorComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	USimpleAnimatorComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	// Métodos para disparar la animación
+	void PlayForward();
+
+	void PlayReverse();
+
+
+	/***COMPONENTS***/
+
+	// Mesh a animar
+	UPROPERTY(EditAnywhere, Category = "Animation") UStaticMeshComponent* TargetMesh;
+
+	// Posiciones de inicio y fin
+	UPROPERTY(EditAnywhere, Category = "Animation") FVector StartLocation;
+
+	UPROPERTY(EditAnywhere, Category = "Animation") FVector EndLocation;
+
+	// Duración total de la animación
+	UPROPERTY(EditAnywhere, Category = "Animation") float AnimationDuration = 1.0f;
+
+
+private:
+	float ElapsedTime = 0.0f;
+	bool bIsAnimating = false;
+	bool bForward = true;
+};
