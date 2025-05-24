164874e62bbfdea8ef2c31265c85dd0fb88d45b3
cabranca <joaquincabrera98@gmail.com>
2025-05-24 01:43:53 -0300

    Added spatialization and attached audio component in Phonograph

--- Source/TrasnochandoConMirna/Private/Phonograph.cpp
+++ Source/TrasnochandoConMirna/Private/Phonograph.cpp
@@ -5,12 +5,14 @@
 APhonograph::APhonograph()
 {
 	ShowOST = CreateDefaultSubobject<UAudioComponent>(TEXT("SFX"));
+	ShowOST->SetupAttachment(RootComponent);
 	ShowOST->bAutoActivate = false;
 	ShowOST->bAllowSpatialization = true;
 
 	ShowOST->bOverrideAttenuation = true;
 
 	ShowOST->AttenuationOverrides.bAttenuate = true;
+	ShowOST->AttenuationOverrides.bSpatialize = true;
 	ShowOST->AttenuationOverrides.AttenuationShape = EAttenuationShape::Sphere;
 	ShowOST->AttenuationOverrides.FalloffDistance = 1500.0f;
 	ShowOST->AttenuationOverrides.AttenuationShapeExtents = FVector(400.f);
