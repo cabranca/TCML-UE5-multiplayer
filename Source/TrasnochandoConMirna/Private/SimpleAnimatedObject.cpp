84d9070769a44b42e6270feffaaee8cce3d50c16
cabranca <joaquincabrera98@gmail.com>
2025-05-25 21:41:19 -0300

    Added replication to object

--- Source/TrasnochandoConMirna/Private/SimpleAnimatedObject.cpp
+++ Source/TrasnochandoConMirna/Private/SimpleAnimatedObject.cpp
@@ -7,12 +7,14 @@ ASimpleAnimatedObject::ASimpleAnimatedObject()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	bReplicates = true;
 
 	Scene = CreateDefaultSubobject<USceneComponent>(TEXT("Scene"));
 	RootComponent = Scene;
 
 	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
 	StaticMesh->SetupAttachment(RootComponent);
+	StaticMesh->SetIsReplicated(true);
 
 	Animator = CreateDefaultSubobject<USimpleAnimatorComponent>(TEXT("Animator"));
 	Animator->TargetMesh = StaticMesh;
