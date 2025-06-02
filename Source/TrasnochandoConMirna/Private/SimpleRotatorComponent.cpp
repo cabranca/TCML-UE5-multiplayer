659f223e348ef3732c513e3320450b4a7d490d13
cabranca <joaquincabrera98@gmail.com>
2025-06-01 23:37:02 -0300

    Added Rotation component

---
+++ Source/TrasnochandoConMirna/Private/SimpleRotatorComponent.cpp
@@ -0,0 +1,62 @@
+#include "SimpleRotatorComponent.h"
+
+USimpleRotatorComponent::USimpleRotatorComponent()
+{
+    PrimaryComponentTick.bCanEverTick = true;
+}
+
+void USimpleRotatorComponent::BeginPlay()
+{
+    Super::BeginPlay();
+
+    if (TargetMesh)
+    {
+        TargetMesh->SetRelativeRotation(bBeginsAtStart ? StartRotation : EndRotation);
+    }
+}
+
+void USimpleRotatorComponent::PlayForward()
+{
+    if (TargetMesh && RotationDuration > 0)
+    {
+        RuntimeStartRotation = TargetMesh->GetRelativeRotation();
+        RuntimeEndRotation = EndRotation;
+
+        bIsRotating = true;
+        bForward = true;
+        ElapsedTime = 0.0f;
+    }
+}
+
+void USimpleRotatorComponent::PlayReverse()
+{
+    if (TargetMesh && RotationDuration > 0)
+    {
+        RuntimeStartRotation = TargetMesh->GetRelativeRotation();
+        RuntimeEndRotation = StartRotation;
+
+        bIsRotating = true;
+        bForward = false;
+        ElapsedTime = 0.0f;
+    }
+}
+
+void USimpleRotatorComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+    if (!bIsRotating || !TargetMesh)
+        return;
+
+    ElapsedTime += DeltaTime;
+    float Alpha = FMath::Clamp(ElapsedTime / RotationDuration, 0.0f, 1.0f);
+
+    FRotator NewRotation = FMath::Lerp(RuntimeStartRotation, RuntimeEndRotation, Alpha);
+
+    TargetMesh->SetRelativeRotation(NewRotation);
+
+    if (Alpha >= 1.0f)
+    {
+        bIsRotating = false;
+    }
+}
---
+++ Source/TrasnochandoConMirna/Public/SimpleRotatorComponent.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "SimpleRotatorComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class TRASNOCHANDOCONMIRNA_API USimpleRotatorComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+    USimpleRotatorComponent();
+
+protected:
+    virtual void BeginPlay() override;
+
+public:
+    virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+    /***COMPONENTS***/
+
+    UPROPERTY(EditAnywhere, Category = "Rotation")  UStaticMeshComponent* TargetMesh; // Mesh to rotate.
+
+    UPROPERTY(EditAnywhere, Category = "Rotation") FRotator StartRotation; // Initial rotator of the rotation.
+
+    UPROPERTY(EditAnywhere, Category = "Rotation") FRotator EndRotation; // End rotator of the rotation.
+
+
+    /***EXPOSED VARIABLES***/
+
+    UPROPERTY(EditAnywhere, Category = "Rotation") float RotationDuration = 1.0f; // Duration of the rotation in seconds.
+
+    UPROPERTY(EditAnywhere, Category = "Animation") bool bBeginsAtStart = true; // Whether the mesh starts at the StartRotation or EndRotation.
+
+
+    /***METHODS***/
+
+    void PlayForward(); // Rotates towards the end.
+
+    void PlayReverse(); // Rotates towards the start.
+
+
+private:
+    float ElapsedTime = 0.0f; // Current time since rotation started.
+    bool bIsRotating = false; // Whether the rotation is in motion or not.
+    bool bForward = true; // Whether the rotation goes from start to end or from end to start.
+    FRotator RuntimeStartRotation; // Start Rotator when rotation is called.
+    FRotator RuntimeEndRotation; // End Rotator when the rotation is called.
+};
