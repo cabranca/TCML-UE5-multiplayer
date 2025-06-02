f17efcbdfe3d3695703cb2b3c1458d4aecd00c0f
cabranca <joaquincabrera98@gmail.com>
2025-06-02 19:42:56 -0300

    Added diary class

---
+++ Source/TrasnochandoConMirna/Public/Diary.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InteractableObject.h"
+#include "Diary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TRASNOCHANDOCONMIRNA_API ADiary : public AInteractableObject
+{
+	GENERATED_BODY()
+
+};
