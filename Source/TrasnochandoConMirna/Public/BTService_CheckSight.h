7caf1d0b16375dbbe1ca338fcf96c28917e8c70e
cabranca <joaquincabrera98@gmail.com>
2025-06-16 18:43:11 -0300

    Added AI CheckSight service

---
+++ Source/TrasnochandoConMirna/Public/BTService_CheckSight.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTService.h"
+#include "BTService_CheckSight.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TRASNOCHANDOCONMIRNA_API UBTService_CheckSight : public UBTService
+{
+	GENERATED_BODY()
+	
+public:
+	UBTService_CheckSight();
+
+protected:
+	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+
+    UPROPERTY(EditAnywhere, Category = "Blackboard")
+	FBlackboardKeySelector TargetActorKey;
+
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	FBlackboardKeySelector LastKnownLocationKey;
+
+    UPROPERTY(EditAnywhere, Category = "Sight")
+    float SightDistance = 1500.f;
+
+    UPROPERTY(EditAnywhere, Category = "Sight")
+    float FieldOfView = 90.f;
+};
